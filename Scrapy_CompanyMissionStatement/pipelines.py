ba4e46a56a49f5895161f9ed2aff19119fc970c9
luckylion777 <always.smile0915@gmail.com>
2018-07-31 00:29:40 -0300

    update pipeline

--- Scrapy_CompanyMissionStatement/pipelines.py
+++ Scrapy_CompanyMissionStatement/pipelines.py
@@ -14,6 +14,8 @@ class CSVPipeline(object):
         return pipeline
 
     def spider_opened(self, spider):
+        if spider.name != 'company_crawler':
+            return
         file = open('result.csv', 'w+b')
         self.files[spider] = file
         self.exporter = CsvItemExporter(file)
@@ -30,10 +32,14 @@ class CSVPipeline(object):
         self.exporter.start_exporting()
 
     def spider_closed(self, spider):
+        if spider.name != 'company_crawler':
+            return
         self.exporter.finish_exporting()
         file = self.files.pop(spider)
         file.close()
 
     def process_item(self, item, spider):
+        if spider.name != 'company_crawler':
+            return item
         self.exporter.export_item(item)
         return item
